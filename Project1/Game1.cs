2154bcb83cbf12139ee644d347e58d6876793b60
Thibaud Evrard <thibaud.evrard@etu.univ-smb.fr>
2023-01-03 11:03:54 +0100

    changement de couleur

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -42,7 +42,7 @@ namespace Project1
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Green);
 
             // TODO: Add your drawing code here
 
