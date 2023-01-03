3a0d5ffc0eb35e29a45ca7f90fb1630df17c4a0b
gallottm <gallottm@D350-S23>
2023-01-03 16:19:29 +0100

    dze

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -43,6 +43,7 @@ namespace Project1
         {
             // TODO: Add your initialization logic here
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
+
             _graphics.PreferredBackBufferWidth = largeurFenetre;
             _graphics.PreferredBackBufferHeight = hauteurFenetre;
             _graphics.ApplyChanges();
@@ -50,9 +51,9 @@ namespace Project1
             // Camera Stuff
             var viewportadapter = new BoxingViewportAdapter(Window, GraphicsDevice, largeurFenetre, hauteurFenetre);
             _camera = new OrthographicCamera(viewportadapter);
-            _cameraPosition = new Vector2(100, 0);
+            _cameraPosition = new Vector2(0, 0);
             _camera.Position = _cameraPosition;
-            _camera.ZoomIn(0.5f);
+            _camera.ZoomIn(0);
             base.Initialize();
         }
 
@@ -68,17 +69,33 @@ namespace Project1
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            System.Console.WriteLine(_camera.Position.X + " " + _camera.Position.Y);
             // TODO: Add your update logic here
-            
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                _camera.Position += new Vector2(-1, 0);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                _camera.Position += new Vector2(1, 0);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                _camera.Position += new Vector2(0, -1);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                _camera.Position += new Vector2(0, 1);
+            }
             _tiledMapRenderer.Update(gameTime);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Transparent);
-
-            var transformMatrix = _camera.GetViewMatrix();
+            GraphicsDevice.Clear(Color.Black);
+            Matrix zoomMatrix = Matrix.CreateScale(1.5f);
+            var transformMatrix = _camera.GetViewMatrix() * zoomMatrix;
             _spriteBatch.Begin(transformMatrix: transformMatrix);
             _tiledMapRenderer.Draw(transformMatrix);
             _spriteBatch.End();
