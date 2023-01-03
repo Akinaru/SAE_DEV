ecadb544cf9e676515bfdff4d756b1ab675ce82d
gallottm <gallottm@D350-S23>
2023-01-03 13:56:48 +0100

    ajout de la tilemap render

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
 
 namespace Project1
 {
@@ -8,6 +10,9 @@ namespace Project1
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        public static TiledMap _tiledMap;
+        public static TiledMapRenderer _tiledMapRenderer;
 
         public Game1()
         {
@@ -19,14 +24,15 @@ namespace Project1
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            _tiledMap = Content.Load<TiledMap>("Map/map");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
             // TODO: use this.Content to load your game content here
         }
 
@@ -36,14 +42,14 @@ namespace Project1
                 Exit();
 
             // TODO: Add your update logic here
-
+            _tiledMapRenderer.Update(gameTime);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
-
+            _tiledMapRenderer.Draw();
 
             base.Draw(gameTime);
         }
