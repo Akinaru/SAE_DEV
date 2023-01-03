9f38dbd3abe68266a300d86d1a8b1eab5fd19b16
gallottm <gallottm@D350-S23>
2023-01-03 17:02:42 +0100

    map gestion

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -3,29 +3,30 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
 using MonoGame.Extended.Sprites;
 using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Renderers;
 using MonoGame.Extended.ViewportAdapters;
+using System.Collections.Generic;
 
 namespace Project1
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        public static GraphicsDeviceManager _graphics;
+        public static SpriteBatch _spriteBatch;
 
         public static TiledMap _tiledMap;
+        public static TiledMap _tiledMapInterieur;
+
         public static TiledMapRenderer _tiledMapRenderer;
 
         public static Vector2 _positionPerso;
         public static AnimatedSprite _perso;
         public static int _vitessePerso;
         public static string animation;
-
-        public int largeurFenetre = 1440;
-        public int hauteurFenetre = 900;
-
 
         public static OrthographicCamera _camera;
         public static Vector2 _cameraPosition;
@@ -37,7 +38,7 @@ namespace Project1
         public static int _mapHeight;
         public static int _mapWidth;
 
-
+        public static List<string> _mapLayers = new List<string>() { "Arbres2" };
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -49,8 +50,10 @@ namespace Project1
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            _vitessePerso = 100;
+
             _screenWidth = 1280;
             _screenHeight = 720;
 
@@ -58,27 +61,34 @@ namespace Project1
             _graphics.PreferredBackBufferHeight = _screenHeight;
             _graphics.ApplyChanges();
 
-            _positionPerso = new Vector2((float)4.5 * 16, 7 * 16);
+            base.Initialize();
+
+
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            //_positionPerso = new Vector2((int)(_screenWidth / 2), _screenHeight / 2);
+            _positionPerso = new Vector2((float)4.5 * _tiledMap.TileWidth, 7 * _tiledMap.TileHeight);
 
             _positionCameraX = _positionPerso.X;
             _positionCameraY = _positionPerso.Y;
-            
             // Camera Stuff
             var viewportadapter = new BoxingViewportAdapter(Window, GraphicsDevice, _screenWidth, _screenHeight);
             _camera = new OrthographicCamera(viewportadapter);
-            _camera.ZoomIn(2f);
-            _cameraPosition = new Vector2(0, 0);
-            _camera.Position = _cameraPosition;
-            _mapWidth = 100 * 16;
-            _mapHeight = 100 * 16;
-            base.Initialize();
+            _cameraPosition = new Vector2(_screenWidth, _screenHeight);
+            _camera.ZoomIn(0.5f);
+
+            _mapWidth = _tiledMap.Width * 16;
+            _mapHeight = _tiledMap.Height * 16;
         }
 
         protected override void LoadContent()
         {
+
+
             _tiledMap = Content.Load<TiledMap>("Map/map");
             _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //SpriteSheet spriteSheet = Content.Load<SpriteSheet>("persoAnimation.sf", new JsonContentLoader());
+            //_perso = new AnimatedSprite(spriteSheet);
 
             // TODO: use this.Content to load your game content here
         }
@@ -87,29 +97,37 @@ namespace Project1
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            System.Console.WriteLine(_camera.Position.X + " " + _camera.Position.Y);
-            // TODO: Add your update logic here
-            if (Keyboard.GetState().IsKeyDown(Keys.Left))
-            {
-                _camera.Position += new Vector2(-1, 0);
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.Right))
-            {
-                _camera.Position += new Vector2(1, 0);
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.Up))
-            {
-                _camera.Position += new Vector2(0, -1);
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.Down))
-            {
-                _camera.Position += new Vector2(0, 1);
-            }
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+            animation = "idle";
+            float walkSpeed = deltaTime * _vitessePerso;
 
+            KeyboardManager.Manage(_positionPerso, _tiledMap, animation, walkSpeed, _mapWidth, _mapHeight, _graphics);
+
+            _positionCameraX = _positionPerso.X;
+            _positionCameraY = _positionPerso.Y;
+
+            if (_positionPerso.X < _screenWidth / 2)
+                _positionCameraX = _screenWidth / 2;
+
+            if (_positionPerso.X > (_mapWidth - _screenWidth / 2))
+                _positionCameraX = (_mapWidth - _screenWidth / 2);
+
+            if (_positionPerso.Y < _screenHeight / 2)
+                _positionCameraY = _screenHeight / 2;
+
+            if (_positionPerso.Y > (_mapHeight - _screenHeight / 2))
+                _positionCameraY = (_mapHeight - _screenHeight / 2);
+
+            _camera.LookAt(new Vector2(_positionCameraX, _positionCameraY));
+
+            //_perso.Play(animation);
+            //_perso.Update(deltaTime);
             _tiledMapRenderer.Update(gameTime);
             base.Update(gameTime);
         }
+
+
 
         protected override void Draw(GameTime gameTime)
         {
@@ -118,9 +136,42 @@ namespace Project1
             _spriteBatch.Begin(transformMatrix: transformMatrix);
 
             _tiledMapRenderer.Draw(transformMatrix);
+            //_spriteBatch.Draw(_perso, _positionPerso);
             _spriteBatch.End();
 
             base.Draw(gameTime);
         }
+
+
+        public static bool IsCollision(ushort x, ushort y)
+        {
+
+
+            for (int i = 0; i < _mapLayers.Count; i++)
+            {
+                TiledMapTileLayer _Layer = _tiledMap.GetLayer<TiledMapTileLayer>(_mapLayers[i]);
+                TiledMapTile? tile;
+                if (_Layer.TryGetTile(x, y, out tile) == false)
+                    return false;
+                if (!tile.Value.IsBlank)
+                    return true;
+
+            }
+            return false;
+
+        }
+
+        public static bool isCollisionPorte(ushort x, ushort y)
+        {
+            TiledMapTileLayer _porte = _tiledMap.GetLayer<TiledMapTileLayer>("Porte");
+            TiledMapTile? tile;
+            if (_porte.TryGetTile(x, y, out tile) == false)
+                return false;
+            if (!tile.Value.IsBlank)
+                return true;
+            return false;
+        }
+
+
     }
 }
---
+++ Project1/KeyboardManager.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class KeyboardManager
+    {
+
+        public static void Manage(Vector2 _positionPerso, TiledMap _tiledMap, string animation, float walkSpeed, int _mapWidth, int _mapHeight, GraphicsDeviceManager _graphics)
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 0.2);
+                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight);
+
+
+                Game1.animation = "walkEast";
+                if (!Game1.IsCollision(tx, ty) && _positionPerso.X < _mapWidth)
+                {
+                    Game1._positionPerso.X += walkSpeed;
+                }
+
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth - 0.2);
+                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight);
+                Game1.animation = "walkWest";
+
+
+                if (!Game1.IsCollision(tx, ty) && _positionPerso.X > 0)
+                {
+                    Game1._positionPerso.X -= walkSpeed;
+                }
+            }
+
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth);
+                ushort ty = (ushort)((_positionPerso.Y - 2) / _tiledMap.TileHeight - 0.2);
+
+                Game1.animation = "walkNorth";
+                if (!Game1.IsCollision(tx, ty) && _positionPerso.Y > 0)
+                {
+
+                    Game1._positionPerso.Y -= walkSpeed;
+                }
+                if (Game1.isCollisionPorte(tx, ty))
+                {
+                    Console.WriteLine("PORTE AU DESSUS");
+                }
+
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth);
+                ushort ty = (ushort)((_positionPerso.Y + 2) / _tiledMap.TileHeight + 0.2);
+
+                Game1.animation = "walkSouth";
+                if (!Game1.IsCollision(tx, ty) && _positionPerso.Y < _mapHeight)
+                {
+
+                    Game1._positionPerso.Y += walkSpeed;
+                }
+
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) && Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                Game1.animation = "idle";
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down) && Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                Game1.animation = "idle";
+            }
+
+
+            if (Keyboard.GetState().IsKeyDown(Keys.M))
+            {
+            }
+        }
+
+    }
+}
