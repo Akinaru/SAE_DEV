52b043a36d9dc18e57e50be0e42403cf059ec451
Akinaru <maxime.utchimata@gmail.com>
2023-01-04 17:19:47 +0100

    ui fix

--- Project1/UI.cs
+++ Project1/UI.cs
@@ -21,11 +21,11 @@ namespace Project1
             {
                 _spriteBatch.Draw(Game1._texturevieCoeurVide, new Vector2(10+ (float)(65 * Game1._viePerso) + 65 * i, 10), Color.White);
             }
-            _spriteBatch.Draw(Game1._textureMonstreUI, new Vector2(1145,20), Color.White);
-            _spriteBatch.DrawString(Game1._police, "0 restants", new Vector2(1200, 35), Color.White);
+            _spriteBatch.Draw(Game1._textureMonstreUI, new Vector2(1125,15), Color.White);
+            _spriteBatch.DrawString(Game1._police, Game1._listeMonstre.Count + " monstres", new Vector2(1180, 30), Color.White);
 
-            _spriteBatch.Draw(Game1._texturePersoUI, new Vector2(1145, 63), Color.White);
-            _spriteBatch.DrawString(Game1._police, "0 kills", new Vector2(1200, 78), Color.White);
+            _spriteBatch.Draw(Game1._texturePersoUI, new Vector2(1125, 58), Color.White);
+            _spriteBatch.DrawString(Game1._police, "0 kills", new Vector2(1180, 73), Color.White);
         }
 
     }
