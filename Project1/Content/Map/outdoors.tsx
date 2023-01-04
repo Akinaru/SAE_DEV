acdc33a12bc4581754eb5dbce6c5de702645c09b
Thibaud Evrard <thibaud.evrard@etu.univ-smb.fr>
2023-01-04 14:35:43 +0100

    map

---
+++ Project1/Content/Map/outdoors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="outdoors" tilewidth="16" tileheight="16" tilecount="841" columns="29">
+ <image source="outdoors.png" width="464" height="464"/>
+</tileset>
