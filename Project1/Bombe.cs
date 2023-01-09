e931db0776c10149cf538b2ff754e8f15b43ccd8
gallottm <gallottm@D350-S23>
2023-01-09 10:35:11 +0100

    bombe

---
+++ Project1/Bombe.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Extended.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Bombe
+    {
+        private AnimatedSprite _bombeSprite;
+        private Vector2 _position;
+        private float _timer;
+
+
+
+    }
+}
