e20cec44f840044168142c980dbddc5d0aea1bdf
gallottm <gallottm@D350-S23>
2023-01-10 11:22:39 +0100

    z

--- Project1/Pause.cs
+++ Project1/Pause.cs
@@ -12,21 +12,29 @@ namespace Project1
     public class Pause
     {
         public static Texture2D _textureblurBackground;
+        public static Texture2D _textureBoutonPause;
         public static Texture2D _textureBoutonMenu;
         public static Texture2D _textureRaccourciM;
+        public static Texture2D _textureRaccourciR;
 
+        public static Vector2 _positionBoutonPause;
         public static Vector2 _positionBoutonReprendre;
         public static Vector2 _positionBoutonMenu;
         public static Vector2 _positionRaccourciM;
+        public static Vector2 _positionRaccourciR;
 
         public static void LoadContent(ContentManager Content){
             _textureblurBackground = Content.Load<Texture2D>("Menu/blurBackground");
             _textureBoutonMenu = Content.Load<Texture2D>("Menu/boutonMenu");
+            _textureBoutonPause = Content.Load<Texture2D>("Menu/pause");
             _textureRaccourciM = Content.Load<Texture2D>("Menu/raccourciTouche/raccourciM");
+            _textureRaccourciR = Content.Load<Texture2D>("Menu/raccourciTouche/raccourciM");
 
+            _positionBoutonPause = new Vector2(1280 / 2 - 250, 720 / 2 - 300);
             _positionBoutonReprendre = new Vector2(1280 / 2 - 350, 720 / 2 + 130);
             _positionBoutonMenu = new Vector2(1280 / 2 - 50 + 100, 720 / 2 + 130);
             _positionRaccourciM = new Vector2(1280 / 2 - 50 + 100, 720 / 2 + 230);
+            _positionRaccourciR = new Vector2(1280 / 2 - 350 + 100, 720 / 2 + 230);
         }
 
         public static void Draw(SpriteBatch _spriteBatch)
@@ -34,9 +42,11 @@ namespace Project1
             if (Jeu._pause)
             {
                 _spriteBatch.Draw(_textureblurBackground, new Vector2(0, 0), Color.White);
+                _spriteBatch.Draw(_textureBoutonPause, _positionBoutonPause, Color.White);
                 _spriteBatch.Draw(_textureBoutonMenu, _positionBoutonReprendre, Color.White);
                 _spriteBatch.Draw(_textureBoutonMenu, _positionBoutonMenu, Color.White);
                 _spriteBatch.Draw(_textureRaccourciM, _positionRaccourciM + new Vector2(150- 9,0), Color.White);
+                _spriteBatch.Draw(_textureRaccourciR, _positionRaccourciR, Color.White);
             }
 
         }
