3f0e19b98003d9cc8f5c1619d78b018e2046b43a
gallottm <gallottm@D350-S23>
2023-01-11 11:39:21 +0100

    z

--- Project1/Monstre.cs
+++ Project1/Monstre.cs
@@ -169,11 +169,7 @@ namespace Project1
                 if(this._mortWait >= 0.3)
                 {
                     Game1._listeMonstre.Remove(this);
-                    if (Game1._listeMonstre.Count == 0 && Game1._listeFantome.Count == 0)
-                    {
-                        Monstre.NewVague(Content);
-                    }
-
+                    Vague.NewVague(Content);
                 }
             }
 
@@ -310,28 +306,6 @@ namespace Project1
                 //monstre.Position += direction * 700 * deltaTime;
             }
         }
-        public static void NewVague(ContentManager Content)
-        {
-            Perso._sonNewVague.Play(Game1._volumeSon, 0, 0);
-            if(Jeu.difficulte == Jeu.NiveauDifficulte.Facile)
-            {
-                Jeu._nombreMonstre += 6;
-            }
-            if (Jeu.difficulte == Jeu.NiveauDifficulte.Difficile)
-            {
-                Jeu._nombreMonstre += 12;
-            }
-            else
-            {
-                Jeu._nombreMonstre += 18;
-            }
-            Jeu._vague += 1;
-            Message.Display("Bravo ! Tu es a la vague "+ Jeu._vague+ ". ", "Les monstres arrivent!", 5);
-            for (int i = 0; i < Jeu._nombreMonstre; i++)
-            {
-                Game1._listeMonstre.Add(new Monstre("monstreAnimation.sf", new Vector2(new Random().Next(0, 1600), new Random().Next(0, 1600)), Content));
-            }
-            
-        }
+
     }
 }
