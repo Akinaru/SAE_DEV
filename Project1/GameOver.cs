fa5e1c47822c88c97f31347c5d3e971b9b95f99b
gallottm <gallottm@D350-S23>
2023-01-11 10:56:22 +0100

    z

--- Project1/GameOver.cs
+++ Project1/GameOver.cs
@@ -115,7 +115,10 @@ namespace Project1
                 Game1._spriteBatch.DrawString(Message._police, "Nombre de kills: " + Jeu._nombreKill, new Vector2(1280 / 2 - 100, 720 / 2 + 20), Color.White);
                 Game1._spriteBatch.DrawString(Message._police, "Nombre de vagues: " + Jeu._vague, new Vector2(1280 / 2 - 100, 720 / 2 + 40), Color.White);
                 Game1._spriteBatch.DrawString(Message._police, "Nombre de coups: " + Jeu._nombreCoup, new Vector2(1280 / 2 - 100, 720 / 2 + 60), Color.White);
-                Game1._spriteBatch.DrawString(Message._police, "Precision: " + Math.Round((Jeu._precision/Jeu._nombreCoup*100),0) + " pourcent", new Vector2(1280 / 2 - 100, 720 / 2 + 80), Color.White);
+                double precision = 0;
+                if(Jeu._nombreCoup > 0)
+                    precision = Math.Round((Jeu._precision / Jeu._nombreCoup * 100), 0);
+                Game1._spriteBatch.DrawString(Message._police, "Precision: " + precision + " pourcent", new Vector2(1280 / 2 - 100, 720 / 2 + 80), Color.White);
             }
             Game1._spriteBatch.End();
         }
